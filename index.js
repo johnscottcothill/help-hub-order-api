2fd06fc5dd73a0e787c574c6727bce28d87a08d3
Jake-Haggett <93196480+Jake-Haggett@users.noreply.github.com>
2026-04-27 14:06:11 +0100

    sort: popularity v1

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ app.post("/sku-search", async (req, res) => {
 
     const gql = `
       query SkuLookup($query: String!) {
-        productVariants(first: 5, query: $query) {
+        productVariants(first: 5, query: $query, sortKey: "POPULAR") {
           edges {
             node {
               sku
