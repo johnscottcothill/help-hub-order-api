5dcf8d3dff5ffbeb7f4c31587e3af00a5263d2b2
Jake-Haggett <93196480+Jake-Haggett@users.noreply.github.com>
2026-05-06 16:55:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -199,8 +199,8 @@ app.post('/order-lookup', async (req, res) => {
     for (const pid of productIds) {
       try {
         const pdata = await shopifyAdminGet(`/products/${pid}.json`);
-        const bomdata = await shopifyAdminGet(`/2026-01/products/${pid}/metafields/93828789780854.json`); // custom.ns_pack_bom_items -- obtain this id via storefront query
-        //const bomdata = "";
+        //const bomdata = await shopifyAdminGet(`/2026-01/products/${pid}/metafields/93828789780854.json`); // custom.ns_pack_bom_items -- obtain this id via storefront query
+        const bomdata = "";
         if (pdata?.product) {
             console.log("bomdata:", bomdata);
 
